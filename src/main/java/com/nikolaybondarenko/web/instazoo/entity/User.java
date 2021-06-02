37359d7236ecfe5a86946585f73b45946da66b12
Nikolay0107 <nimphonoe@gmail.com>
2021-06-02 16:15:09 +0300

    Added CustomUserDetailsService and JWTTokenProvider

--- src/main/java/com/nikolaybondarenko/web/instazoo/entity/User.java
+++ src/main/java/com/nikolaybondarenko/web/instazoo/entity/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.nikolaybondarenko.web.instazoo.entity.enums.ERole;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -12,7 +13,7 @@ import java.util.*;
 
 @Data //ломбок аннотация. создаем геттеры и сеттеры для данного объекта
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,7 +34,7 @@ public class User {
     @ElementCollection(targetClass = ERole.class)
     @CollectionTable(name = "user_role",
         joinColumns = @JoinColumn(name = "user_id"))
-    private Set<ERole> role = new HashSet<>();
+    private Set<ERole> roles = new HashSet<>();
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
@@ -48,8 +49,52 @@ public class User {
     public User(){
     }
 
+    public User(Long id,
+                String username,
+                String email,
+                String password,
+                Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
     @PrePersist // аннотация задает значение атрибута до того, как мы сделаем новую записьв бд
     protected void onCreate(){
         this.createdDate = LocalDateTime.now();
+    }
+
+    /**
+     * SECURITY
+     */
+
+
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
